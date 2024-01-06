109a62e9f6c9ff57ce4286c4fd5acc25d27cc705
jokw1228 <jokw1228@naver.com>
2024-01-06 20:05:08 +0900

    PlayerDeadBody Velocity 수정

--- Scenes/Player/DeadBody/PlayerDeadBody.gd
+++ Scenes/Player/DeadBody/PlayerDeadBody.gd
@@ -3,14 +3,14 @@ extends Node2D
 var velocity_vector = Vector2.ZERO
 
 func _ready():
-	await get_tree().create_timer(3.0).timeout
+	await get_tree().create_timer(1.0).timeout
 	queue_free()
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
 	if velocity_vector.length() > 0:
-		velocity_vector = velocity_vector.normalized() * (velocity_vector.length() - 1 * delta)
-	position += velocity_vector
+		velocity_vector = velocity_vector.normalized() * (velocity_vector.length() - 100 * delta)#수정해야함
+	position += velocity_vector * delta
 
 func set_velocity(player_velocity):
-	velocity_vector = player_velocity * -1 * 10
+	velocity_vector = player_velocity * -1 * 0.3
