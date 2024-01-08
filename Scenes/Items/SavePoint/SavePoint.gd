e1e99cc95d4a1acdead334d5e65fd18eb32c6de6
jokw1228 <jokw1228@naver.com>
2024-01-08 22:26:24 +0900

    SavePointSaved 추가

--- Scenes/Items/SavePoint/SavePoint.gd
+++ Scenes/Items/SavePoint/SavePoint.gd
@@ -3,7 +3,8 @@ extends Area2D
 @export var SavePointSprite_node: Sprite2D
 @export var CircleSprite_node: AnimatedSprite2D
 
-@export var SavePointSaved_scene: PackedScene
+#@export var SavePointSaved_scene: PackedScene
+var SavePointSaved_scene = load("res://Scenes/Items/SavePoint/SavePointSaved.tscn")
 
 var theta = 0
 
@@ -22,5 +23,5 @@ func _on_area_entered(area):
 	if area.is_in_group("PlayerArea2D"):
 		var inst = SavePointSaved_scene.instantiate()
 		inst.position = position
-		get_tree().current_scene.add_child(inst)
+		get_tree().current_scene.add_child.call_deferred(inst)
 		queue_free()
--- Scenes/Items/SavePoint/SavePointSaved.gd
+++ Scenes/Items/SavePoint/SavePointSaved.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @export var SavePoint_node: Sprite2D
 
-@export var SavePoint_scene: PackedScene
+#@export var SavePoint_scene: PackedScene
+var SavePoint_scene = load("res://Scenes/Items/SavePoint/SavePoint.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +12,7 @@ func _ready():
 		await get_tree().create_timer(0.1).timeout
 		SavePoint_node.modulate = Color(1, 1, 1, 1)
 		await get_tree().create_timer(0.1).timeout
-	#var inst = SavePoint_scene.instantiate()
-	#inst.position = position
-	#get_tree().current_scene.add_child(inst)
+	var inst = SavePoint_scene.instantiate()
+	inst.position = position
+	get_tree().current_scene.add_child.call_deferred(inst)
 	queue_free()
