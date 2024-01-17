eb7d008d8a9cffaa2083571453b88ed3deea4edf
jokw1228 <jokw1228@naver.com>
2024-01-17 18:20:36 +0900

    SwitchBlockLightEffectGenerator 수정

--- Scenes/Items/SwitchBlock/SwitchBlockLightEffectGenerator.gd
+++ Scenes/Items/SwitchBlock/SwitchBlockLightEffectGenerator.gd
@@ -6,5 +6,9 @@ extends Node2D
 func _ready():
 	Sprite_node.play()
 	Audio_node.play()
-	await get_tree().create_timer(1.0).timeout
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(Sprite_node, "modulate", Color(1, 1, 1, 0), 10/Sprite_node.speed_scale)
+	
+	await tween.finished
 	queue_free()
