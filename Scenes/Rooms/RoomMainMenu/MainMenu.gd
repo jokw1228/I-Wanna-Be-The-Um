77ada3944efb66094def18dbe374fea2b28eff89
jokw1228 <jokw1228@naver.com>
2024-01-23 22:31:11 +0900

    MainMenu 효과음 추가

--- Scenes/Rooms/RoomMainMenu/MainMenu.gd
+++ Scenes/Rooms/RoomMainMenu/MainMenu.gd
@@ -5,6 +5,9 @@ extends Control
 @export var StartGame_node: Label
 @export var EndGame_node: Label
 
+@export var audio1: AudioStreamPlayer2D
+@export var audio2: AudioStreamPlayer2D
+
 @export var DemoScene_room: PackedScene
 
 var state = 0;
@@ -14,27 +17,33 @@ func _input(event):
 		if event is InputEventKey:
 			if event.pressed:
 				state = 1
+				light_out(PressAnyKey_node)
 				var tween = get_tree().create_tween()
-				tween.tween_property(PressAnyKey_node, "modulate", Color(1, 1, 1, 0), 0.1)
-				var tween2 = get_tree().create_tween()
-				tween2.tween_property(SelectMenu_node, "modulate", Color(1, 1, 1, 1), 0.1)
+				tween.tween_property(SelectMenu_node, "modulate", Color(1, 1, 1, 1), 0.1)
 				light_off(EndGame_node)
+				audio1.play()
 				pass
 	elif state == 1:
 		if Input.is_action_just_pressed("down"):
 			state = 2
 			light_off(StartGame_node)
 			light_on(EndGame_node)
+			audio2.play()
 		elif Input.is_action_just_pressed("jump"):
 			state = 3
+			light_out(StartGame_node)
+			audio1.play()
 			RoomTransitionManager.room_change(RoomTransitionEffectManager.type.up_to_down, true, DemoScene_room, RoomTransitionEffectManager.type.up_to_down, 0)
 	elif state == 2:
 		if Input.is_action_just_pressed("up"):
 			state = 1
 			light_on(StartGame_node)
 			light_off(EndGame_node)
+			audio2.play()
 		elif Input.is_action_just_pressed("jump"):
 			state = 3
+			light_out(EndGame_node)
+			audio1.play()
 			RoomTransitionEffectManager.effecting(RoomTransitionEffectManager.type.up_to_down, true)
 			await RoomTransitionEffectManager.effecting_end
 			get_tree().quit()
@@ -50,3 +59,7 @@ func light_off(node: Node):
 	tween.tween_property(node, "modulate", Color(1, 1, 1, 0.3), 0.1)
 	var tween2 = get_tree().create_tween()
 	tween2.tween_property(node, "scale", Vector2(0.8, 0.8), 0.1)
+
+func light_out(node: Node):
+	var tween = get_tree().create_tween()
+	tween.tween_property(node, "scale", Vector2(1.5,0), 0.05)
